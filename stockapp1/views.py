212df82b078ff6608460b05de7039306ad495a1c
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-04 13:39:35 -0400

    username + home.html

--- stockapp1/views.py
+++ stockapp1/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+def home( request):
+    context = { 'username': 'Ian Wehmeyer'}
+    return render( request, 'stockapp1/home.html', context)
